a291ebbffaab2d9fb7a2497f7488ee58526a2679
ANORA\vinoth <vinoth@anorasolutions.com>
2023-03-21 16:24:02 +0530

    print statement two times

---
+++ 111.py
@@ -0,0 +1,30 @@
+rows=6
+for i in range(rows):
+    for j in range(rows-i):
+        print("*",end="")
+    print("\r")
+l=[1,2,3,4,5,6,7]
+for i in range(len(l)+1):
+    if i%2:
+        print(i)
+for i in range(2,len(l),2):
+    print(i)
+
+for item in range(6,1,-1):
+    print("*"*item)
+l=[1,2,3,4,5,9,6,100,.7,55]
+l9=[]
+max=l[0]
+for i in l:
+    if i>max:
+        max=i
+print(max)
+print(i)
+
+
+
+
+
+print(i)
+
+
